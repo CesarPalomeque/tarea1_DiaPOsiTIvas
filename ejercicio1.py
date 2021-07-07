bf17e83dff024f1ae3da9f5f5a2bf587014d46f4
CesarPalomeque <85848931+CesarPalomeque@users.noreply.github.com>
2021-07-07 11:38:55 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+class algoritmo():
+
+    def  __init__ (self):
+        pass
+
+    def  ejercicio1 (self):              #EJERCICIO 1
+        radio=  float ( input ("ingrese el radio del círculo:"))
+        superficie =  3.1416 * ( radio **2 )
+        print ("La superficie del círculo es {}".format(superficie))
+
+clase1 =  algoritmo ()
+clase1 . ejercicio1 ()
+          
